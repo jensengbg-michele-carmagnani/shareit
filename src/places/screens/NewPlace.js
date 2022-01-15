cb29125c9a19107013b2678a683c502865d9a7c4
Michele Carmagnani <michele.carmagnani@gmail.com>
2022-01-15 10:30:42 +0100

    add git ignore

--- src/places/screens/NewPlace.js
+++ src/places/screens/NewPlace.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import Input from '../../shared/components/UI/Input/Input.js'
-
-import "./NewPlace.css"
+import React from 'react';
+import Input from '../../shared/components/UI/Input/Input.js';
+import { VALIDATOR_REQUIRE } from '../../shared/utils/validators';
+import './NewPlace.css';
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" lable="Tile" />
+      <Input
+        element="input"
+        type="text"
+        lable="Tile"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+      />
     </form>
-  )
-}
+  );
+};
 
-export default NewPlace
+export default NewPlace;
--- src/shared/components/UI/Input/Input.js
+++ src/shared/components/UI/Input/Input.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './Input.css';
+import { validate } from '../../../utils/validators';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case 'TOUCH': {
+      return { ...state, isTouched: true };
+    }
+    default:
+      return state;
+  }
+};
+
 const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false,
+  });
+  const touchHandler = () => {
+    // @ts-ignore
+    dispatch({ type: 'TOUCH' });
+  };
+  const changeHandler = (event) => {
+    // @ts-ignore
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
   const element =
     props.element === 'input' ? (
       <input
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
-        onChange={props.onChange}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
       />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea id={props.id} rows={props.rows || 3} value={inputState.value} />
     );
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && 'form-control--invalid'
+      }`}
+    >
       <label htmlFor={props.id}>{props.lable}</label>
       {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
